3968f1f261842b5e99bbfb3398d08ada557b0993
sara <sara.abraham12@gmail.com>
2025-04-03 17:44:04 +0200

    Added HOME variable

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# Install Maven
+
+# Install Maven (if not already installed)
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install maven
 
-# Run the Maven build
+# Install Java (or set JAVA_HOME for pre-installed Java)
+sdk install java 17.0.7-tem    # Install Java 17 (you can choose another version)
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+
+# Verify Java and Maven installations
+java -version
+mvn -version
+
+# Run Maven build
 mvn clean package
+
